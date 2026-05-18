70f04f0ed02f05eea23b7a09c646ee5256edec67
shimami <oshima.m0805@gmail.com>
2026-05-18 14:44:22 +0900

    Git管理の開始

---
+++ practice/app.js
@@ -0,0 +1,23 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+
+
+let names = ['John', 'Bob', 'Michael', 'Emma'];
+
+console.log('names[0] => ', names[0]);
+console.log('names[1] => ', names[1]);
+console.log('names[2] => ', names[2]);
+console.log('names[3] => ', names[3]);
+console.log('names.length => ', names.length);
+
+
+let John = {
+  name: 'John', // key（キー）: value（バリュー）
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+};
